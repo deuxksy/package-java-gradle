49c68e254e5bf8fd42683616e10144db372fedd1
SeokYoung.kim <deuxksy@gmail.com>
2019-10-08 19:57:07 +0900

    gradle wrapper 5.6.2-bin

---
+++ src/main/java/com/zzizily/GradleProperties.java
@@ -0,0 +1,22 @@
+package com.zzizily;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class GradleProperties {
+
+  private static Properties properties = new Properties();
+
+  static {
+    try {
+      properties.load(new FileInputStream("gradle.properties"));
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+  }
+
+  public static String getProperty(String key) {
+    return properties.getProperty(key);
+  }
+}
--- src/main/java/com/zzizily/Util.java
+++
@@ -1,7 +0,0 @@
-package com.zzizily;
-
-public class Util {
-  public static String m1(String a) {
-    return a;
-  }
-}
